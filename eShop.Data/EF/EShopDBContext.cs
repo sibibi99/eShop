d3b39fc8fec34b78f0ac0e5a2fc57492e97157d5
singsang <sibibi99@gmail.com>
2021-07-10 13:26:30 +0700

    Create Migrations Database

--- eShop.Data/EF/EShopDBContext.cs
+++ eShop.Data/EF/EShopDBContext.cs
@@ -1,4 +1,5 @@
-﻿using eShop.Data.Entities;
+﻿using eShop.Data.Configurations;
+using eShop.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -6,11 +7,33 @@ using System.Text;
 
 namespace eShop.Data.EF
 {
-    public class EShopDBContext : DbContext
+    public class EShopDbContext : DbContext
     {
-        public EShopDBContext(DbContextOptions options) : base(options)
+        public EShopDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CartConfiguration());
+
+            modelBuilder.ApplyConfiguration(new AppConfigConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductInCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new OrderConfiguration());
+
+            modelBuilder.ApplyConfiguration(new OrderDetailConfiguration());
+            modelBuilder.ApplyConfiguration(new CategoryTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new ContactConfiguration());
+            modelBuilder.ApplyConfiguration(new LanguageConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductTranslationConfiguration());
+            modelBuilder.ApplyConfiguration(new PromotionConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            //base.OnModelCreating(modelBuilder);
+        }
+
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 
@@ -35,6 +58,8 @@ namespace eShop.Data.EF
 
 
         public DbSet<Transaction> Transactions { get; set; }
+
+
 
     }
 }
---
+++ eShop.Data/EF/EShopDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShop.Data.EF
+{
+    public class EShopDbContextFactory : IDesignTimeDbContextFactory<EShopDbContext>
+    {
+        public EShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                //Add thu muc nay lam thu muc goc va add file JSON
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.js")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("eShopDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<EShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new EShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
